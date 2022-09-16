73187107c538232b86e0a3888ede6e817f44ae8a
vagunda <vagunda@edhouse.cz>
2022-09-16 10:19:19 +0200

    crud

--- src/main/java/com/example/demojpa/data/UserRepository.java
+++ src/main/java/com/example/demojpa/data/UserRepository.java
@@ -2,8 +2,9 @@ package com.example.demojpa.data;
 
 import com.example.demojpa.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Integer> {
+public interface UserRepository extends CrudRepository<User,Integer> {
 }
